822199a623f4a7dce2ffad28842162cb6d06f252
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-27 00:47:02 +0300

    Fixed shopping

--- app/package/shopping.py
+++ app/package/shopping.py
@@ -11,16 +11,18 @@ def buy_products(customer: Customer, shops: list[Shop]) -> list:
 
     for i in range(len(shops)):
         milk_price = customer.product_cart["milk"] * shops[i].products["milk"]
+        if milk_price % 1 == 0:
+            milk_price = int(milk_price)
+
         bread_price = (customer.product_cart["bread"]
                        * shops[i].products["bread"])
+        if bread_price % 1 == 0:
+            bread_price = int(bread_price)
+
         butter_price = (customer.product_cart["butter"]
                         * shops[i].products["butter"])
-
-        milk_price = int(milk_price) if milk_price.is_integer() else milk_price
-        bread_price = int(bread_price) if bread_price.is_integer() \
-            else bread_price
-        butter_price = int(butter_price) if butter_price.is_integer() \
-            else butter_price
+        if butter_price % 1 == 0:
+            butter_price = int(butter_price)
 
         total_price = milk_price + bread_price + butter_price
         shops_prices.append([milk_price,
